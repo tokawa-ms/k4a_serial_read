95d160f1bfb0fcd258d748764d62e221023cb289
Takashi Okawa <tokawa@microsoft.com>
2021-01-29 22:54:53 +0900

    プロジェクト ファイルを追加します。

---
+++ k4a_test_001/k4a_test_001.cpp
@@ -0,0 +1,37 @@
+﻿#pragma comment(lib, "k4a.lib")
+#include <k4a/k4a.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    uint32_t count = k4a_device_get_installed_count();
+    if (count == 0)
+    {
+        printf("No k4a devices attached!\n");
+        return 1;
+    }
+
+    // Open the first plugged in Kinect device
+    k4a_device_t device = NULL;
+    if (K4A_FAILED(k4a_device_open(K4A_DEVICE_DEFAULT, &device)))
+    {
+        printf("Failed to open k4a device!\n");
+        return 1;
+    }
+
+    // Get the size of the serial number
+    size_t serial_size = 0;
+    k4a_device_get_serialnum(device, NULL, &serial_size);
+
+    // Allocate memory for the serial, then acquire it
+    char* serial = (char*)(malloc(serial_size));
+    k4a_device_get_serialnum(device, serial, &serial_size);
+    printf("Opened device: %s\n", serial);
+    free(serial);
+
+    k4a_device_close(device);
+
+    return 0;
+}
